e3986dda5bcf596406ba7b64c1809d72fb57fd9e
Anton <anton.moroz@helsinki.fi>
2021-03-08 23:14:24 +0200

    11.9 Simple ent to end -tests work locally per task instructions

--- cypress/integration/pokemon_app.test.js
+++ cypress/integration/pokemon_app.test.js
@@ -5,5 +5,13 @@ describe('Pokedex', function () {
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+
+    cy.contains('chlorophyll').should('not.exist')
+  })
+  it('navigation should be possible to another page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+
+    cy.contains('chlorophyll')
   })
 })
