9e4e30bbeb6e181f936c85ce70118573606b28da
Anton <anton.moroz@helsinki.fi>
2021-03-13 14:05:40 +0200

    Test to see whether the token still applies

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('OK!')
+  res.send('Okay')
 })
 
 app.get('/version', (req, res) => {
